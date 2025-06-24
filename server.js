3922c450ae7779b69223e173a0bc224803aeb095
sarahxb <86785453+sarahxb@users.noreply.github.com>
2025-06-24 05:22:58 +0300

    docum

--- server.js
+++ server.js
@@ -232,7 +232,7 @@ const server = http.createServer(async (req, res) => {
                 return;
             }
         } else {
-            // User not logged in - serve the main landing page
+           
             console.log('User not logged in - serving index.html');
             fs.readFile('./public/views/index.html', (err, data) => {
                 if (err) {
